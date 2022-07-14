3339f47ee055947fc6dd824c40a51a0ed431c333
jgs731 <jgs73@live.co.uk>
2022-07-14 22:01:56 +0100

    Initial boilerplate code added to class - Main class inherits AudioPluginBase - Added metadata in constructor method - Implemented base methods Initialise and Process as these methods form the basis of most plugins developed using this library -TODO: Add first parameter and make release build to check if post-build steps work and plugin works within a DAW

--- JSPlugin/Program.cs
+++ JSPlugin/Program.cs
@@ -2,8 +2,40 @@
 
 namespace JSPlugin
 {
-    public class Program
+    public class Program : AudioPluginBase
     {
-                
+        public Program()
+        {
+            Company = "MUC";
+            Website = "www.jg-tests.co.uk";
+            Contact = "contact@my.email";
+            PluginName = "JS Bass Plugin";
+            PluginCategory = "Instrument|Synth";
+            PluginVersion = "1.0.0";
+
+            // Unique 64bit ID for the plugin
+            PluginID = 0x94477AF90D2F50BD;
+        }
+
+        AudioIOPort monoInput;
+        AudioIOPort monoOutput;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            InputPorts = new AudioIOPort[] { monoInput = new AudioIOPort("Mono Input", EAudioChannelConfiguration.Mono) };
+            OutputPorts = new AudioIOPort[] { monoOutput = new AudioIOPort("Mono Output", EAudioChannelConfiguration.Mono) };
+
+            AddParameter(new AudioPluginParameter
+            {
+                // Add first parameter from document for JSPlugin
+            });
+        }
+
+        public override void Process()
+        {
+            base.Process();
+        }
     }
 }
